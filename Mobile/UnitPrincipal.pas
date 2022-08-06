fdab56cabe5db3ede56194b1e35ec9112604f9b0
Dario Maracaja <dariomaracaja410@gmail.com>
2022-08-06 20:22:48 -0300

    tela login/cadastro

---
+++ Mobile/UnitPrincipal.pas
@@ -0,0 +1,29 @@
+unit UnitPrincipal;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.Objects, FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TFrmPrincipal = class(TForm)
+    LytToolbar: TLayout;
+    Image1: TImage;
+    Image2: TImage;
+    Label1: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmPrincipal: TFrmPrincipal;
+
+implementation
+
+{$R *.fmx}
+
+end.
